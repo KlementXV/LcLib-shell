addd18528a55aec018d21cc392cb299708da024b
Clément <50872057+clementlvx@users.noreply.github.com>
2022-02-01 18:23:49 +0100

    Update Classic_Debian_postInstall.sh

--- Classic_Debian_postInstall.sh
+++ Classic_Debian_postInstall.sh
@@ -12,18 +12,17 @@ Main() {
     LcLib_update_ssh 22 clm
     LcLib_install_docker
     LcLib_install_dockerCompose
-
-    if [ "$2" != "1" ]; then
-        if [ "$1" != "-keep" ]; then rm ./LcLib-shell.sh; exit 0; fi
-    fi
 }
 
 if test -f ./LcLib-shell.sh; then
     Main $1 "1"
 else
     if [[ `wget -S --spider "${LINK_LCLIB}" 2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
-        wget ${LINK_LCLIB}
-        sleep 5
+        echo -e -ne "\033[0;36mDOWNLOAD LIBRARY - \033[0m \r"
+        sudo sh -c "wget ${LINK_LCLIB} && sleep 5" 2> /dev/null
+        echo -e -ne "\033[0;36mDOWNLOAD LIBRARY - DONE\033[0m \r"
+        echo -ne '\n'
         Main $1 "0"
     fi
 fi
+if [ "$2" != "1" ]; then if [ "$1" != "-keep" ]; then rm ./LcLib-shell.sh; exit 0; fi; fi
