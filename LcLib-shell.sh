72a25f7824f1c857d7ab929ff8abc2fcc94ebb8b
Clément <50872057+clementlvx@users.noreply.github.com>
2022-02-01 22:10:09 +0100

    Update LcLib-shell.sh

--- LcLib-shell.sh
+++ LcLib-shell.sh
@@ -187,7 +187,7 @@ ScriptName=`basename "$0" .sh`
         LcLib_execNull "mkdir ~/.ssh"
         LcLib_execNull "chmod 700 ~/.ssh"
         for i in ${SSH_KEYS}; do
-            LcLib_execNull "wget -qO - '${LINK_SSH_KEYS}${i}'| cat >> ~/.ssh/authorized_keys && echo blop"
+            LcLib_execNull "wget -qO - '${LINK_SSH_KEYS}${i}'| tee -a ~/.ssh/authorized_keys"
         done
         LcLib_execNull "chmod 600 ~/.ssh/authorized_keys"
         LcLib_execNull "wget -O /etc/issue.net ${LINK_SSH_BANNER}"
